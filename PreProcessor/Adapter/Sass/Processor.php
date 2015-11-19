ce180ef992067bdecf16634d2a3520f84bcf3ea5
James Anelay <jamesanelay@gmail.com>
2015-11-19 01:04:41 +0000

    add PreProcessor/Adapter/Sass/Processor.php

--- PreProcessor/Adapter/Sass/Processor.php
+++ PreProcessor/Adapter/Sass/Processor.php
@@ -2,13 +2,37 @@
 
 namespace TheExtensionLab\SassPreProcessor\PreProcessor\Adapter\Sass;
 
+use Magento\Framework\View\Asset\ContentProcessorException;
 use Magento\Framework\View\Asset\File;
 use Magento\Framework\View\Asset\ContentProcessorInterface;
+use Magento\Framework\View\Asset\Source;
+use Psr\Log\LoggerInterface;
 
 class Processor implements ContentProcessorInterface
 {
+    private $logger;
+
+    public function __construct(
+        Source $assetSource,
+        LoggerInterface $logger
+    ) {
+        $this->assetSource = $assetSource;
+        $this->logger = $logger;
+    }
+
     public function processContent(File $asset)
     {
+        $path = $asset->getPath();
+        try {
+            $scssIn = $this->assetSource->getContent($asset);
 
+            $compiler = new \Leafo\ScssPhp\Compiler;
+            $cssOut = $compiler->compile($scssIn);
+
+            return $cssOut;
+        } catch (\Exception $e) {
+            $errorMessage = PHP_EOL . self::ERROR_MESSAGE_PREFIX . PHP_EOL . $path . PHP_EOL . $e->getMessage();
+            $this->logger->critical($errorMessage);
+        }
     }
 }
--- Test/Unit/PreProcessor/Adapter/Sass/ProcessorTest.php
+++ Test/Unit/PreProcessor/Adapter/Sass/ProcessorTest.php
@@ -6,11 +6,105 @@ use Magento\Framework\View\Asset\ContentProcessorInterface;
 
 class ProcessorTest extends \PHPUnit_Framework_TestCase
 {
+    const ERROR_MESSAGE = 'Test exception';
+    const ASSET_PATH = 'test-path';
+
     private $preProcessor;
+    private $assetMock;
+    private $assetSourceMock;
+    private $loggerMock;
+
+    protected function setUp()
+    {
+        $this->assetMock = $this->getMockBuilder('Magento\Framework\View\Asset\File')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->assetSourceMock = $this->getMockBuilder('Magento\Framework\View\Asset\Source')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->loggerMock = $this->getMockBuilder('Psr\Log\LoggerInterface')
+            ->getMockForAbstractClass();
+
+        $this->preProcessor = new Processor(
+            $this->assetSourceMock,
+            $this->loggerMock
+        );
+    }
 
     public function testClassImplementsContentProcessorInterface()
     {
-        $this->preProcessor = new Processor;
         $this->assertInstanceOf(ContentProcessorInterface::class,$this->preProcessor);
+    }
+
+    public function testNoAssetContentReturnsNoContent()
+    {
+        $this->assetSourceMock->expects(self::once())
+            ->method('getContent')
+            ->with($this->assetMock)
+            ->willReturn('');
+
+        $cssOutput = $this->preProcessor->processContent($this->assetMock);
+        $this->assertEquals('', $cssOutput);
+    }
+
+    public function testExceptionIsLoggedWithinGetContent()
+    {
+        $this->assetMock->expects(self::once())
+            ->method('getPath')
+            ->willReturn(self::ASSET_PATH);
+
+        $this->assetSourceMock->expects(self::once())
+            ->method('getContent')
+            ->with($this->assetMock)
+            ->willThrowException(new \Exception(self::ERROR_MESSAGE));
+
+        $this->loggerMock->expects(self::once())
+            ->method('critical')
+            ->with(
+                PHP_EOL . Processor::ERROR_MESSAGE_PREFIX . PHP_EOL . self::ASSET_PATH  . PHP_EOL . self::ERROR_MESSAGE
+            );
+
+        $this->preProcessor->processContent($this->assetMock);
+    }
+
+    public function testBasicContentStringIsReturnedTheSame()
+    {
+        $expected = "body{background-color:red;}";
+
+        $this->assetSourceMock->expects(self::once())
+            ->method('getContent')
+            ->with($this->assetMock)
+            ->willReturn('body{background-color:red;}');
+
+        $cssOutput = $this->preProcessor->processContent($this->assetMock);
+        $strippedCssOutput = $this->minifiCss($cssOutput);
+        $this->assertEquals($expected, $strippedCssOutput);
+    }
+
+    public function testBasicSassIsProcessedCorrectly()
+    {
+        $expected = 'body{background-color:#fff;}';
+
+        $mockContent = '$primary-color: #fff;
+
+        body {
+          background-color:$primary-color;
+        }';
+
+        $this->assetSourceMock->expects(self::once())
+            ->method('getContent')
+            ->with($this->assetMock)
+            ->willReturn($mockContent);
+
+        $cssOutput = $this->preProcessor->processContent($this->assetMock);
+        $strippedCssOutput = $this->minifiCss($cssOutput);
+        $this->assertEquals($expected, $strippedCssOutput);
+    }
+
+    private function minifiCss($css){
+        $minifiedCss = str_replace(' ','',preg_replace('/\s+/S', " ", $css));
+        return $minifiedCss;
     }
 }
